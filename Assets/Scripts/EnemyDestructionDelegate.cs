784f938771ab9281a2986b991f303f651d23dcbb
Thamsta <sven.willrodt@me.com>
2016-11-04 23:48:26 +0100

    cleanup

--- Assets/Scripts/EnemyDestructionDelegate.cs
+++ Assets/Scripts/EnemyDestructionDelegate.cs
@@ -6,16 +6,6 @@ public class EnemyDestructionDelegate : MonoBehaviour {
     public delegate void EnemyDelegate(GameObject enemy);
     public EnemyDelegate enemyDelegate;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void OnDestroy()
     {
         if (enemyDelegate != null)
---
+++ Assets/Scripts/ExplosionSelfDestructor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ExplosionSelfDestructor : MonoBehaviour
+{
+
+    private float spawnTime;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - spawnTime > 1)
+        {
+           Destroy(gameObject);
+        }
+    }
+}
