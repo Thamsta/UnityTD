150224f9f7d9b188df1bfc08dead2920fdd71ba7
Thamsta <sven.willrodt@me.com>
2016-12-25 20:48:15 +0100

    portal

---
+++ Assets/_Scripts/AutoRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+
+    public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(new Vector3(0.0f, rotateSpeed, 0.0f));
+	}
+}
